7ec9524347ac21e07f84afe820102b79ae8b509b
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-02-09 13:56:18 +0100

    adding all files

---
+++ index.js
@@ -0,0 +1,11 @@
+/*import express from "express";
+
+
+const app = express();
+
+
+const PORT = 3070;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+*/
