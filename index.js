1cf2ea68379c24a39395ce189a6ec7c113bf2b9d
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-02-10 14:27:15 +0100

    adding partials and home router

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import ejs from "ejs";
-
+import homeRoutes from "./routes/home.js";
 const app = express();
 const port = 3070;
 
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("pages/index", { title: "Indian Bistro - Home" });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
