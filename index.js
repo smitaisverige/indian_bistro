822f7512c224557d04359654378ff9641d9e4205
Rob <robert.chamberlain@cmeducations.se>
2026-02-11 14:15:31 +0100

    Adding a home page response

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = 3070;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Indian Bistro")
+})
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
